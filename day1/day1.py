fdb9670525acab5d45e0deff094d96ec4b6b9f98
Tom Sitter <thomas.sitter@gmail.com>
2022-11-30 22:05:20 -0800

    cleanup functions

--- day1/day1.py
+++ day1/day1.py
@@ -1,8 +1,18 @@
 from argparse import ArgumentParser
 from pathlib import Path
 
-def read_input(filename):
-    with open(filename) as f:
+
+def get_data(example=False):
+    """
+    Reads a file with the same name as this script and returns the data as a list of lines.
+    Adds _example to the filename if example is True.
+    """
+    cwd = Path(__file__).parent
+    filename =  Path(__file__).stem + "_example" if example else Path(__file__).stem
+
+    data_file = (cwd / filename).with_suffix('.txt')
+
+    with open(data_file) as f:
         input = f.read().splitlines()
 
     return input
@@ -27,19 +37,12 @@ def part2(calories):
     print("Part 2")    
     print(sum(sorted(calories, reverse=True)[:3]))
 
-
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--example', action='store_true')
     args = parser.parse_args()
 
-    filename =  Path(__file__).stem
-    if args.example:
-        filename += '_example.txt'
-    else:
-        filename += '.txt'
-
-    data = read_input(filename)
+    data = get_data(args.example)
     calories = get_calories_per_elf(data)
 
     part1(calories)
